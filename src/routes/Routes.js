69a85a8db47018d5e2bc73ee0f8ceeabef9ae104
Axel Giovanni Ojeda Hernandez <aojedah1800@alumno.ipn.mx>
2025-04-06 16:59:45 -0600

    router cambio

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Inicio from '../components/Inicio'
 import ReservasGenerales  from '../components/ReservasGenerales';
+import Taquilla from '../components/Taquilla';
 
 const AppRoutes = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -11,8 +12,7 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/inicio" element={<Inicio />} />
                 <Route path="/ReservasGenerales" element={<ReservasGenerales />} />
-
-
+                <Route path='taquilla' element={<Taquilla />} />
                 <Route path="/" element={<Inicio />} />
                 <Route path="*" element={<Navigate to="/inicio" />} />
             </Routes>
