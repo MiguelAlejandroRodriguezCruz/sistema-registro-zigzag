d926ead6d8102912a592ee08ff54c5b05d90372b
OmarFdezC <128112619+OmarFdezC@users.noreply.github.com>
2025-09-02 19:13:59 -0600

    Vista LoginAdmin

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -9,6 +9,7 @@ import ListaEventos from '../components/ListaEventos';
 import EventosVisitantes from '../components/EventosVisitantes';
 import EventosDescripcion from '../components/EventosDescripcion';
 import Login from '../components/Login/Login';
+import LoginAdmin from '../components/Login/LoginAdmin';
 import Registro from '../components/Login/Registro'
 import NuevaContrasena from '../components/Login/NuevaContrasena';
 import SolicitarCodigo from '../components/Login/SolicitarCodigo';
@@ -33,6 +34,7 @@ const AppRoutes = () => {
                 <Route path="/eventos-visitantes" element={<PrivateRoute><EventosVisitantes /></PrivateRoute>} />
                 <Route path="/eventos-descripcion/:id" element={<PrivateRoute><EventosDescripcion /></PrivateRoute>} />
                 <Route path='/login' element={<Login />} />
+                <Route path='/login-admin' element={<LoginAdmin />} />
                 <Route path='/registro' element={<Registro />} />
                 <Route path='/nueva-contrasena' element={<NuevaContrasena />} />
                 <Route path='/solicitar-codigo' element={<SolicitarCodigo />} />
