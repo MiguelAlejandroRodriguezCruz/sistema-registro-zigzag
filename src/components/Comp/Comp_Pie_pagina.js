d1c7055e0582af03f6a19dc0ba86895074f5f190
DesireeCG <kami3haru@gmail.com>
2025-09-23 18:33:12 -0600

    Header_and_footer

--- src/components/Comp/Comp_Pie_pagina.js
+++ src/components/Comp/Comp_Pie_pagina.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from "../../images/zig_zag_logo.png";
 
 export function Comp_Pie_pagina() {
@@ -8,6 +7,16 @@ export function Comp_Pie_pagina() {
         <div className="pie-logo-texto">
           <img src={logo} alt="Logo Zig Zag" className="pie-logo" />
           <span className="pie-derechos">&copy; 2025 Zig Zag. Todos los derechos reservados.</span>
+        </div>
+        {/* Nombres del equipo */}
+        <div className="pie-equipo">
+          <h4>Equipo del proyecto:</h4>
+          <ul>
+            <li>Miguel A. Rodríguez Cruz</li>
+            <li>Axel G. Ojeda Hernández</li>
+            <li>Desireé Castañeda García</li>
+            <li>Carlos O. Fernández Casillas</li>
+          </ul>
         </div>
         <div className="pie-enlaces">
           <a href="#">Aviso legal</a>
--- src/components/Comp/Comp_encabezado.js
+++ src/components/Comp/Comp_encabezado.js
@@ -1,17 +1,34 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from "react-router-dom";
 import logo from "../../images/zig_zag_logo.png";
+import logo_labsol from "../../images/Logos_Labsol_2023_02.png";
+import logo_GPL from "../../images/GPLv3_Logo.svg.png";
 
 export function Comp_encabezado() {
   return (
     <>
-      <header className=" encabezado container d-flex align-items-center m-1">
-        <img
-          src={logo}
-          alt="Logo Zig Zag"
-          className='encabezado__logo'
-          style={{ height: '60px', cursor: 'pointer' }}
-        />
+      <header className=" comp_encabezado">
+        <a href="https://zigzag.gob.mx/" target="_blank" rel="noopener noreferrer"> 
+          <img
+            src={logo}
+            alt="Logo Zig Zag"
+            className='encabezado__logo logo-left' 
+          />
+        </a>
+        <a href="https://labsol.cozcyt.gob.mx/" target="_blank" rel="noopener noreferrer">
+          <img 
+            src={logo_labsol}
+            alt="Logo Labsol Network"
+            className='encabezado__logo logo-center '
+          />
+        </a>
+        
+        <a href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank" rel="noopener noreferrer">
+          <img 
+            src={logo_GPL}
+            alt="Logo GPL3 Free Software"
+            className='encabezado__logo logo-right'
+          />
+        </a>
+       
       </header>
       
 
