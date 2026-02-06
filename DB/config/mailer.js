f62da2d978b93e8894209a7f2afed2c4353bdca5
AxelOjedaHernandez <axelojedahernandez64@gmail.com>
2026-02-06 12:32:49 -0600

    correcion archivos config del backend

--- DB/config/mailer.js
+++ DB/config/mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.EMAIL_USER || 'yomiguel250@gmail.com',
-        pass: process.env.EMAIL_PASS || 'ehkr vhjy lxsl lppc' // Contraseña de aplicación
+        user: process.env.EMAIL_USER ,
+        pass: process.env.EMAIL_PASS
     },
     tls: {
         rejectUnauthorized: false
